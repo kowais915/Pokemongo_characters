5079233cab1d9bb1f63ff08a0d245f97eff4335b
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-03-31 03:49:34 +0500

    :recycle: added a card component

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+    return ( 
+        <div className="card">
+            <h1>
+                This is the card component.
+            </h1>
+        </div>
+     );
+}
+ 
+export default Card;
